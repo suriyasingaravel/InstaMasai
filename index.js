6b743ed381501022d974aede43c4d3c0dbe40e00
suriyasingaravel <suriyasingaravel710.com>
2023-11-01 00:03:52 +0530

    cors

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const cors= require("cors");
 const {BlacklistModel} = require("./Model/blaklist.model")
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/posts",postRouter);
-app.use(cors());
+
 
 app.get("/",(req,res)=>{
     res.status(200).send({"msg":"Welcome to my website"});
